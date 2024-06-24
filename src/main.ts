21cba4afb35a3d68aea176d6dc88dfa5b83aed98
olzhas alimbayev <olzhastype@user>
2024-06-24 17:34:53 +0500

    Add temp

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+
+import Aura from '@primevue/themes/aura'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
 
 app.mount('#app')
+
+app.component('Button', Button)
+app.component('DataTable', DataTable)
+app.component('Column', Column)
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="card">
+    <DataTable :value="products" tableStyle="min-width: 50rem">
+      <Column field="time" header="Time"></Column>
+      <Column field="temperature" header="Temperature"></Column>
+    </DataTable>
   </div>
 </template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { ProductService } from '../stores/service';
 
+
+const products = ref<any>([]);
+
+onMounted(async () => {
+  await ProductService.getWeather().then(data => {
+    console.log(data.hourly);
+
+    for (let i = 0; i < data.hourly.time.length; i++) {
+      products.value.push({
+        time: data.hourly.time[i],
+        temperature: data.hourly.temperature_2m[i],
+      });
+    }
+  });
+});
+
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
