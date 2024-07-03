262fe414e5edf296a149fb5dda4ea3668fb8ef6d
olzhas alimbayev <olzhastype@user>
2024-07-03 14:02:38 +0500

    #master Add auth widget

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,6 +1,15 @@
-<template>
-    <div> <Alert message="Hello!" /></div>
-</template>
-<script setup lang="ts">
-import { Alert } from 'vue-tg'
+<script lang="ts" setup>
+import { LoginWidget } from 'vue-tg'
+import type { LoginWidgetUser } from 'vue-tg'
+
+function handleUserAuth(user: LoginWidgetUser) {
+  // ...
+}
 </script>
+
+<template>
+  <LoginWidget 
+    bot-username="samplebot"
+    @auth="handleUserAuth"
+  />
+</template>
