e95bbcfd0c88475e8f5ce60a44f857c91d6c8898
olzhas alimbayev <olzhastype@user>
2024-07-03 13:58:36 +0500

    #master *Add qr view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,16 @@ const goTo = () => {
   console.log('click')
   router.push('/auth')
 }
-
+const goToQr = () => {
+  console.log('click')
+  router.push('/qr')
+}
 </script>
 
 <template>
   <div>
-    <div @click="goTo">Click</div>
+    <div @click="goTo">Click Auth</div>
+    <div @click="goToQr">Click Qr</div>
   <RouterView />
 </div>
 </template>
---
+++ src/views/QrView.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import { ScanQr } from 'vue-tg'
+const router = useRouter()
+const  handleScanResult =(data: string) =>{
+  router.push({name:'auth'});
+}
+</script>
+
+<template>
+  <ScanQr @result="handleScanResult" />
+</template>
